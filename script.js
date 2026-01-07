36ceb8b750fc631b5c3bda4c67b04f41d1b6e4bc
Ibrahim <tamelatiecoumbaibrahim@gmail.com>
2026-01-07 17:06:39 +0000

    premier commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Smooth scrolling and simple fade-in animations
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portfolio Loaded");
+    
+    // You can add logic here to dynamically load projects from a JSON file later!
+});
